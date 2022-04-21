031d9d4cd3c660ae187c02a3adebc4753d71fe54
unknown <inesachour@insat.u-carthage.tn>
2022-04-21 16:44:08 +0200

    loop

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,52 +1,49 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import TinderCard from 'react-tinder-card';
-import { Carousel } from "react-responsive-carousel";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function PhotoCard({objets}) {
 
-  //const [objet, setObjet] = useState(objets[0])
-
+  //index of shown image
   const [index, setIndex] = useState(1);
   
-  React.useEffect(()=>{
+  useEffect(()=>{
     setIndex(1);
   },[])
 
+  //incrementing the index of shown after swiping
   function swiper() {
     setIndex(prev => prev+1);
   }
 
+  //returning the card
   return (
     objets.map((objet) => {
       if(objet.id == index)
       {
-      return (
-        <div className="cardStyles">
-        <TinderCard
-          preventSwipe={["up", "down"]}
-          className="Cards"
-          onSwipe={swiper}
-        >
-          <div>
-              <img alt="Images of web apps" src={objets[index].url} />
-          </div>
-          
-          <p className="techList">
-              {objets[index].title}
-          </p>
+        return (
+          <div className="cardStyles">
+          <TinderCard
+            preventSwipe={["up", "down"]}
+            key = {objets[index].id}
+            className="Cards"
+            onSwipe={swiper}
+          >
             
-        </TinderCard>
-        </div>
-    
-    );
-  }
-  }) 
+            <CardMedia>
+                <img alt="Images of web apps" src={objets[index].url} />
+            </CardMedia>
+            
+            <Typography className="techList">
+                {objets[index].title}
+            </Typography>
+              
+          </TinderCard>
+          </div>
+        );
+  }}) 
   );
 }
